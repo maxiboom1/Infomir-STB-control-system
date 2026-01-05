4fa42853b5edc3f019372d26e7e383cc799600c3
Alex Samih-zade <galutziogen@gmail.com>
2026-01-05 15:38:11 +0200

    added addr line in boot console

--- src/3-utilities/logger-messages.js
+++ src/3-utilities/logger-messages.js
@@ -1,5 +1,6 @@
 import logger from "./logger.js";
 import appConfig from "./app-config.js";
+import os from "os";
 
 class Messages {
 
@@ -7,9 +8,25 @@ class Messages {
         console.clear();
         logger(`**********************************************************************`,"blue");
         logger(`[SYSTEM] Infomir_STB_control_system, App Version: ${appConfig.version}, now starting...`,"green");
+        logger(`[SYSTEM] App is available on http://www.${getLanIPv4()}:${appConfig.appPort}`,"green");
+
+
         logger(`**********************************************************************`,"blue");
     }
 
+}
+
+function getLanIPv4() {
+    const nets = os.networkInterfaces();
+  
+    for (const name of Object.keys(nets)) {
+      for (const net of nets[name] || []) {
+        // Node 18: net.family can be 'IPv4' or 4 (depending on platform)
+        const isIPv4 = net.family === "IPv4" || net.family === 4;
+        if (isIPv4 && !net.internal) return net.address; // first non-internal IPv4
+      }
+    }
+    return "localhost";
 }
 
 const logMessages = new Messages();
